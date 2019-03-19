d14f9cba1147d8d1f1afc72d38ee2da20fae2820
Bruno Pertini <brunopertini@gmail.com>
2019-03-19 18:46:33 -0300

    another approach to google maps issue

--- project/src/main-office/main-office.service.ts
+++ project/src/main-office/main-office.service.ts
@@ -54,66 +54,68 @@ export class MainOfficeService {
       return this.warehouseService.warehouses_cities();
   }
 
-  private onMatrix(err, distances) {
-    var auxArray = [];
-    var sortedDistances = [];
-    var resultArray=[];
-    var results;
-
-    if (!err && distances.status == 'OK') {
-      //If no error, for each origin it's distance to the destiny is stored in auxArray
-      results = distances['rows'].map(d => d['elements']);
-      var mappedDistances = results.map(r=>r[0]['distance']);
-      for(var i=0;i<origins.length;++i){
-          auxArray[origins[i]] = mappedDistances[i]['value']; 
-          sortedDistances.push(mappedDistances[i]['value']);
-      }
-
-      //Once all distances are taken, they're sorted ascending, and finally associated to it's city
-      sortedDistances.sort(function(a, b){return a-b});
-      var i = 0;
-      while (i < 10) {
-          Object.keys(auxArray).forEach(key => {
-              if(auxArray[key] == sortedDistances[i]) {    
-                  resultArray.push( {
-                      city: key,
-                      distance: Math.floor(sortedDistances[i]/1000)
-                  });
-
-                  i = i + 1;
-              }
-          });     
-      }   
-    }
-      console.log('INSIDE FUNCTION: '+JSON.stringify(resultArray));
-      return resultArray;
-  }
-
-
-  /**
+  
+   /**
    * FIXME: as this is returning an empty array as result, despite inside matrix callback function the
    * result array has the expected data, this array will return mocked data.
    * @param cityName 
    * @returns a list of the closest warehouses cities to the given city, each one with it's distance in kilometers.
    */
-   closests_warehouses_for_city(cityName:string){
-    var distance = require('google-distance-matrix');
-    distance.key(apiKey);
-    return  distance.matrix(origins, [cityName], this.onMatrix);
-    
-    // resultArray = [
-    //     {"cityName":"Salta","distance":196},
-    //     {"cityName":"San Miguel de Tucumán","distance":228},
-    //     {"cityName":"Córdoba, Argentina","distance":705.},
-    //     {"cityName":"Santa Fe, Argentina","distance":950},
-    //     {"cityName":"Mendoza","distance":1064},
-    //     {"cityName":"Rosario","distance":1102},
-    //     {"cityName":"Buenos Aires","distance":1393},
-    //     {"cityName":"La Plata","distance":1451},
-    //     {"cityName":"Mar del Plata","distance":1805},
-    //     {"cityName":"Trelew","distance":2148}
-    // ];
+  async closests_warehouses_for_city(cityName:string){
+    const googleMapsClient = require('@google/maps').createClient({
+      key: apiKey,
+    });
+
+    // return await googleMapsClient.distanceMatrix({origins: origins,destinations: [cityName]},
+    //   function(err,distances) {
+    //     var auxArray = [];
+    //     var sortedDistances = [];
+    //     var resultArray=[];
+    //     var results;
+
+    //     //If no error, for each origin it's distance to the destiny is stored in auxArray
+    //     results = distances['json']['rows'].map(d => d['elements']);
+    //     var mappedDistances = results.map(r=>r[0]['distance']);
+    //     for(var i=0;i<origins.length;++i){
+    //         auxArray[origins[i]] = mappedDistances[i]['value']; 
+    //         sortedDistances.push(mappedDistances[i]['value']);
+    //     }
+
+    //     //Once all distances are taken, they're sorted ascending, and finally associated to it's city
+    //     sortedDistances.sort(function(a, b){return a-b});
+    //     var i = 0;
+    //     while (i < 10) {
+    //         Object.keys(auxArray).forEach(key => {
+    //             if(auxArray[key] == sortedDistances[i]) {    
+    //                 resultArray.push( {
+    //                     city: key,
+    //                     distance: Math.floor(sortedDistances[i]/1000)
+    //                 });
+
+    //                 i = i + 1;
+    //             }
+    //         });     
+    //     }   
+    //     //console.log('INSIDE FUNCTION: '+JSON.stringify(resultArray));
+    //     return resultArray;  
+    //   });
+     
+    var resultArray = [
+        {"cityName":"Salta","distance":196},
+        {"cityName":"San Miguel de Tucumán","distance":228},
+        {"cityName":"Córdoba, Argentina","distance":705.},
+        {"cityName":"Santa Fe, Argentina","distance":950},
+        {"cityName":"Mendoza","distance":1064},
+        {"cityName":"Rosario","distance":1102},
+        {"cityName":"Buenos Aires","distance":1393},
+        {"cityName":"La Plata","distance":1451},
+        {"cityName":"Mar del Plata","distance":1805},
+        {"cityName":"Trelew","distance":2148}
+    ];
+
+    return resultArray;
   }
+
 
   async send_package_to_city(destiny: string) {
     var warehousesCities = await this.closests_warehouses_for_city(destiny);
